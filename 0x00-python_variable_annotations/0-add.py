1b5c114054de1b895b2de14b21194d24a18563c7
Ay-slim <ayooluwaadedipe@gmail.com>
2024-01-11 18:50:21 +0000

    Add documentation

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,4 +2,10 @@
 
 
 def add(a: float, b: float) -> float:
+    """
+    add: Function to add two floats and return their sum
+    @a: first float
+    @b: second float
+    returns: Float (sum of a and b)
+    """
     return a + b
