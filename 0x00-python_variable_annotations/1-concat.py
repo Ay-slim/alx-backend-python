1ca52c9240ad67b64b45f5fa8df288e15bd7581c
Ay-slim <ayooluwaadedipe@gmail.com>
2024-01-11 18:55:09 +0000

    Concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""1-concat py module"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    concat: Function to add two strings and return their concatenation
+    @str1: first string
+    @str2: second string
+    returns: string
+    """
+    return str1 + str2
