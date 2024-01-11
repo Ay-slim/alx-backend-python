8245e9e3ee08c197e2f1c9c1310bb25c6abc8582
Ay-slim <ayooluwaadedipe@gmail.com>
2024-01-11 19:16:00 +0000

    Float to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+"""To string function module"""
+
+
+def to_str(n: float) -> str:
+    """
+    to_str: Converts a float to string
+    @n: Float to convert
+    Returns: A string
+    """
+    return str(n)
