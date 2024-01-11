62a92199765f7383f5170274090d65ec1bb0652f
Ay-slim <ayooluwaadedipe@gmail.com>
2024-01-11 20:51:04 +0000

    Make multiplier

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -6,4 +6,9 @@ from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    make_multiplier - Make multiplier
+    @multiplier: Multiplier to work with
+    Return: Returns a function multiplying the arg with a float
+    """
     return lambda x: x * multiplier
