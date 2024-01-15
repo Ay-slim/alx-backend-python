c1e32a0399820bd6e1ac73892ed12b25f65675b7
Ay-slim <ayooluwaadedipe@gmail.com>
2024-01-15 12:04:36 +0000

    Document wait_n function

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,6 +8,12 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    wait_n - Function to execute the wait_random function a given number of times asynchronously
+    @n: Int number of times to run the wait_random function
+    @max_delay: Int argument to the wait_random function
+    Returns: List of floats
+    """
     ret_list = []
     for i in range(n):
         ret_list.append(await wait_random(max_delay))
